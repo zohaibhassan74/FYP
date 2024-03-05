195686be39e0df3f0e1fe02a287d2e09bda4fdbb
Zohaib hassan <zohaibkashif@gmail.com>
2024-03-05 09:49:34 +0500

    Handle errors and user models

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,9 @@
 require('dotenv').config()
-import express from 'express'
+import express, { Request,Response,NextFunction } from 'express'
 export const app = express()
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
+import { ErrorMiddleware } from './middleware/error'
 
 app.use(express.json({limit: "50mb"}))
 
@@ -14,3 +15,19 @@ app.use(cookieParser())
 app.use(cors({
     origin: process.env.ORIGIN
 }))
+
+
+app.get("/test",(req:Request,res:Response,next:NextFunction)=>{
+    res.status(200).json({
+        success:true,
+        message:"API is working"
+    })
+})
+
+app.all("*",(req:Request,res:Response,next:NextFunction)=>{
+    const err = new Error(`Route ${req.originalUrl} not found`) as any;
+    err.statusCode = 404
+    next(err)
+} )
+
+app.use(ErrorMiddleware)
--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,8 @@
 import { app } from "./app";
 require("dotenv").config()
+import connectDB from "./utils/db";
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is connected with the PORT ${process.env.PORT}`)
+     connectDB()
 })
