f9cce80a43502182de0b1af7b860cc5e29b79cd1
Zohaib hassan <zohaibkashif@gmail.com>
2024-03-24 23:53:29 +0500

    Header and Hero

---
+++ client/redux/features/store.ts
@@ -0,0 +1,22 @@
+"use client";
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./api/apiSlice";
+import authSlice from "./auth/authSlice";
+
+export const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authSlice,
+  },
+  devTools: false,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+});
+
+const initializeApp = async () => {
+  await store.dispatch(
+    apiSlice.endpoints.loadUser.initiate({}, { forceRefetch: true })
+  );
+};
+
+initializeApp();
--- server/app.ts
+++ server/app.ts
@@ -16,7 +16,7 @@ app.use(express.json({limit: "50mb"}))
 app.use(cookieParser())
 
 app.use(cors({
-    origin: process.env.ORIGIN
+    origin: ["http://localhost:3000"],
 }))
 
 app.use("/api/v1", userRouter)
